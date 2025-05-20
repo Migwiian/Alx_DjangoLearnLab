4573b672d93c8c517402d6f99cc04272c4164f99
ian-migwi <ismigwi@gmail.com>
2025-05-20 14:12:47 +0300

    Added file basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number_1=10
+number_2=5
+sum_result=number_1+number_2
+difference_result=number_1-number_2
+product_result=number_1*number_2
+print("Addition", "of", number_1, "and", number_2, "is", sum_result)
+print("Subtraction", "of", number_1, "and", number_2, "is", difference_result)
+print("Multiplication", "of", number_1, "and", number_2, "is", product_result)
