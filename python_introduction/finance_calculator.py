7b35167d372cf1928abb9c2700e5b03f401a0033
ian-migwi <ismigwi@gmail.com>
2025-05-25 20:12:29 +0300

    "Capitalization"

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income=float(input("Enter your monthly income: "))
 total_monthly_expenses=float(input("Enter your total monthly expenses: "))
 Monthly savings=monthly_income-total_monthly_expenses
-Projected savings = (Monthly savings * 12) + (Monthly savings * 12 * 0.05)
+Projected Savings = (Monthly savings * 12) + (Monthly savings * 12 * 0.05)
 print("Your monthly savings are: ", Monthly savings)
-print("Projected savings after one year, with interest, is:", Projected savings)
+print("Projected Savings after one year, with interest, is:", Projected savings)
 
