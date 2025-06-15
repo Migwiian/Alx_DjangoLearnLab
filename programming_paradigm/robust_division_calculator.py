b831fb61c8218898ce92f4b0b0fa501c836b5e04
ian-migwi <ismigwi@gmail.com>
2025-06-15 23:17:41 +0300

    Practice at object instantiation, and method invocation.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,9 +4,7 @@ def safe_divide(numerator, denominator):
         den = float(denominator)
         result = num / den
         return f"The result of the division is {result}"
-
     except ValueError:
         return "Error: Please enter numeric values only."
-
     except ZeroDivisionError:        
         return "Error: Cannot divide by zero."
