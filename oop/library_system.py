b59c67cae451f5762c49172216c5aeb8a9c39159
ian-migwi <ismigwi@gmail.com>
2025-06-22 22:47:14 +0300

    Made correction on file

--- oop/library_system.py
+++ oop/library_system.py
@@ -11,7 +11,8 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"__str__(self):
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"__str__(self):
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -19,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+         return f"EBook: {self.title} by {self.author}, Page Count: {self.file_size}KB"
 
 class Library:
     def __init__(self, name="Default_lib"):
