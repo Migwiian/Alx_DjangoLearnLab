9fb8d5f999f84141652ac817f8f8bf4babd35e8b
ian-migwi <ismigwi@gmail.com>
2025-07-20 23:07:17 +0300

    Query changes6

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,21 +6,31 @@ if not Author.objects.exists():
     Book.objects.create(title="Harry Potter", author=jk)
     central_library = Library.objects.create(name="Central Library")
     Librarian.objects.create(name="Mr. Kinuthia", library=central_library)
-    
     harry_potter_book = Book.objects.get(title="Harry Potter")
     central_library.books.add(harry_potter_book)
 
-author = Author.objects.get(name="J.K. Rowling")
-print(f"Books by {author.name}:")
-for book in author.books.all():
+author_name = "J.K. Rowling" 
+author_obj = Author.objects.get(name=author_name) 
+books_by_author = Book.objects.filter(author=author_obj) 
+
+print(f"Books by {author_obj.name}:")
+for book in books_by_author:
     print(f"- {book.title}")
 
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)
-print(f"Books in {library.name}:")
-for book in library.books.all():
+library_name = "Central Library" 
+library_obj = Library.objects.get(name=library_name) 
+
+print(f"Books in {library_obj.name}:")
+for book in library_obj.books.all(): 
     print(f"- {book.title}")
 
-librarian_name = "Mr. Kinuthia"
-librarian = Librarian.objects.get(name=librarian_name, library=library)
-print(f"Librarian for {library.name}: {librarian.name}")
+librarian_name_to_find = "Mr. Kinuthia" 
+library_for_librarian_query = Library.objects.get(name="Central Library") 
+
+librarian_query_set = Librarian.objects.filter(name=librarian_name_to_find, library=library_for_librarian_query)
+librarian_obj = librarian_query_set.first()
+
+if librarian_obj:
+    print(f"Librarian for {library_for_librarian_query.name}: {librarian_obj.name}")
+else:
+    print(f"No librarian named {librarian_name_to_find} found for {library_for_librarian_query.name}.")
