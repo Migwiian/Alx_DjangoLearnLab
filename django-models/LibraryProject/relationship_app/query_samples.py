43e078f2c88f4b3703a0ccc10e39cdc987f04b16
ian-migwi <ismigwi@gmail.com>
2025-07-20 21:54:55 +0300

    Added some details to the query.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,21 +6,24 @@ if not Author.objects.exists():
     jk = Author.objects.create(name="J.K. Rowling")
     Book.objects.create(title="Harry Potter", author=jk)
     lib = Library.objects.create(name="Central Library")
-    Librarian.objects.create(name="Ms. Smith", library=lib)
+    Librarian.objects.create(name="Mr. Kinuthia", library=lib)
     lib.books.add(Book.objects.first())
 
 # 1. Query all books by a specific author
-author = Author.objects.first()
+author = :J.K. Rowling
+author = Author.objects.get(name="J.K. Rowling")
 print(f"\n1. Books by {author.name}:")
 for book in author.books.all():
     print(f"- {book.title}")
 
 # 2. List all books in a library
-library = Library.objects.first()
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 print(f"\n2. Books in {library.name}:")
 for book in library.books.all():
     print(f"- {book.title}")
 
 # 3. Retrieve the librarian for a library
-librarian = Librarian.objects.first()
+librarian = "Mr. Kinuthia"
+librarian = Librarian.objects.get(name=librarian, library=library)
 print(f"\n3. Librarian for {library.name}: {librarian.name}")
