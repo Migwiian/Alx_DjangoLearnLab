f5e84431a7da4b29e05d0632de3541473dd13fce
ian-migwi <ismigwi@gmail.com>
2025-07-20 23:53:40 +0300

    Views tweaking1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.views.generic.list import ListView
 from .models import Book, 
 from .models import Library
 
