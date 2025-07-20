f9d250d990ec8c7c4891c246d3acdf22f7ec4f71
ian-migwi <ismigwi@gmail.com>
2025-07-21 00:50:35 +0300

    Tweaked views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.list import ListView
 from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 
