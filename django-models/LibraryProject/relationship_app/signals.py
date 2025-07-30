bada638c2fa352d3714a1e5b9f980dcc501cd9d8
ian-migwi <ismigwi@gmail.com>
2025-07-30 13:01:37 +0300

    Edited receiver function

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -4,13 +4,32 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Signal receiver function to automatically create or update a UserProfile
+    whenever a User object is saved.
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'userprofile'):
-        instance.userprofile.save()
-    else:
+    Args:
+        sender: The model class that sent the signal (User in this case).
+        instance: The actual instance of the User that was saved.
+        created (bool): True if a new record was created, False if an existing record was updated.
+        kwargs: Additional keyword arguments.
+    """
+    if created:
+        # If a new User was just created, create a corresponding UserProfile.
+        # The 'role' field will default to 'Member' as defined in your UserProfile model.
         UserProfile.objects.create(user=instance)
+    else:
+        # If an existing User was saved (not created), ensure its UserProfile is also saved.
+        # This is important if you modify UserProfile fields directly and then save the User.
+        # The hasattr check prevents errors if, for some reason, a User doesn't have a profile yet
+        # (e.g., from old data or specific test setups before profiles were mandatory).
+        if hasattr(instance, 'userprofile'):
+            instance.userprofile.save()
+        # else:
+        #   If you reach here, it means an existing User was saved, but it somehow
+        #   doesn't have a UserProfile. This is an edge case that usually indicates
+        #   a data inconsistency. The 'create' logic handles new users, so for existing
+        #   users without a profile, you might want to log an error or handle it
+        #   differently depending on your application's requirements.
+        #   For most cases, the 'if created' block ensures all new users get a profile.
