c9938e54b7f17483463bd229ee82ba79bb07af69
ian-migwi <ismigwi@gmail.com>
2025-07-30 12:51:50 +0300

    Created views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('books/', views.book_list, name='book-list'),
+    path('books/', views.list_books, name='book-list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
