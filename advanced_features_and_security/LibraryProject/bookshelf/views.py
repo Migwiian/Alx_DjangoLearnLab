f434c6a39121e90ed34c84cc7c933ffab5fffff1
ian-migwi <ismigwi@gmail.com>
2025-08-07 14:38:29 +0300

    Added CRUD view and permission enhancement

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,18 +1,51 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
 
-def is_admin_group_member(user):
-    return user.groups.filter(name='Admin').exists()
+from .models import Book
+from .forms import BookForm
 
-@user_passes_test(is_admin_group_member, login_url='login')
-def admin_view(request):
-    return render(request, 'admin_view.html', {'message': 'Welcome to the admin view!'})
-
-# Create your views here.
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.add_book', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    
+    return render(request, 'bookshelf/create_book.html', {'form': form})
+
+@permission_required('bookshelf.change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+        
+    return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
+
+@permission_required('bookshelf.delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
