53f36686b3237a0159b5b1eed9a0d758566948fc
ian-migwi <ismigwi@gmail.com>
2025-08-10 13:22:16 +0300

    feat: implement and test CRUD endpoints for books

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('api/', include router.urls),  # Include the router URLs   
 ]
