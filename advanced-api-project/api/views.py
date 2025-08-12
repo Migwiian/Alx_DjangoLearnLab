66a6bea7758c506c494915c8fad6ee023c976374
ian-migwi <ismigwi@gmail.com>
2025-08-12 11:20:21 +0300

    Imported rest_framework permissions for authentification

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, 
+from rest_framework permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
