9f26d87a15288b90ae2ba7e17274fbdc8a7e4692
ian-migwi <ismigwi@gmail.com>
2025-08-12 11:07:31 +0300

    Modified custom views names

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/books/", ListView.as_view(), name="book-list"),
+    path("api/books/<int:pk>/", DetailView.as_view(), name="book-detail"),
+    path("api/books/create/", CreateView.as_view(), name="book-create"),
+    path("api/books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
+    path("api/books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
+]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class ListView(generic.ListView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class DetailView(generic.RetrieveView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CreateView(generic.CreateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UpdateView(generic.UpdateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class DeleteView(generic.DestroyView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
