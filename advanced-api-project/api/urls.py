4644393bbce7c7a5af5da3bc1e714900ba4bdbba
ian-migwi <ismigwi@gmail.com>
2025-08-12 11:09:06 +0300

    modified url path names

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/books/", ListView.as_view(), name="book-list"),
-    path("api/books/<int:pk>/", DetailView.as_view(), name="book-detail"),
-    path("api/books/create/", CreateView.as_view(), name="book-create"),
-    path("api/books/<int:pk>/update/", UpdateView.as_view(), name="book-update"),
-    path("api/books/<int:pk>/delete/", DeleteView.as_view(), name="book-delete"),
+    path("api/books/", ListView.as_view(), name="books-list"),
+    path("api/books/<int:pk>/", DetailView.as_view(), name="books-detail"),
+    path("api/books/create/", CreateView.as_view(), name="books-create"),
+    path("api/books/<int:pk>/update/", UpdateView.as_view(), name="books-update"),
+    path("api/books/<int:pk>/delete/", DeleteView.as_view(), name="books-delete"),
 ]
