4c67f5854ea80ce7d67f29e0ef3499d94b625734
ian-migwi <ismigwi@gmail.com>
2025-08-17 20:25:01 +0300

    Added comment CRUD operations url paths

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('post/', views.ListPostsView.as_view(), name='post_list'),  # ✅ .as_view() added  
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),  
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update/', views.UpdatePostView.as_view(), name='post_update'),  
     path('post/<int:pk>/delete/', views.DeletePostView.as_view(), name='post_delete'),  
+    path('comment/<int:pk>/comments/new/', views.CreateCommentView.as_view(), name='comment_new'),
+    path('comment/<int:pk>/update/', views.UpdateCommentView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.DeleteCommentView.as_view(), name='comment_delete'),
 
     
 ]
